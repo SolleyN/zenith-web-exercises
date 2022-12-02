8027681c76a68e9e76a4e9b509d21e7294bce12d
Nickolas Solley <nicksolley@gmail.com>
2022-12-02 16:11:00 -0600

    working on arrays

--- js/iterating.js
+++ js/iterating.js
@@ -6,36 +6,36 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-let names = ["Janice", "Chris", "Jon", "Nick"];
+const peoplesNames = ["Janice", "Chris", "Jon", "Nick"];
 
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-console.log(names);
+console.log(peoplesNames.length);
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-console.log(names [0]);
-console.log(names [1]);
-console.log(names [2]);
-console.log(names [3]);
+// console.log(peoplesNames [0]);
+// console.log(peoplesNames [1]);
+// console.log(peoplesNames [2]);
+// console.log(peoplesNames [3]);
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-    for (let i = 0; i < names.length; i += 1) {
-        console.log(names [i])}
+    for (let i = 0; i < peoplesNames.length; i ++) {
+        console.log(peoplesNames [i])};
         /**
          * TODO:
          * Refactor your above code to use a `forEach` loop
          */
-        names.forEach(function (names) {
-            console.log(names)});
+        peoplesNames.forEach(function (name) {
+            console.log(name)});
         /**
          * TODO:
          * Create the following three functions, each will accept an array and
@@ -49,14 +49,18 @@ console.log(names [3]);
          *  > second([1, 2, 3, 4, 5]) // returns 2
          *  > last([1, 2, 3, 4, 5]) // return 5
          */
+let numArr =[1, 2, 3, 4, 5];
 
-function  returnFirst([]) {
-    return [0];
+function  first(arr) {
+    return arr[0];
         }
-        function returnTwo([]) {
-     return [1];
+        function second(arr) {
+     return arr[1];
         }
-        function returnLast([]) {
-    return [5];
+        function last(arr) {
+    return arr[arr.length - 1 ];
 
-        }
+        };
+    console.log(first(numArr));
+    console.log(second(numArr));
+    console.log(last(numArr));
