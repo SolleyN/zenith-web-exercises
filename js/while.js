de38c84629c324130420c7d69ea4b22d6508831d
Nickolas Solley <nicksolley@gmail.com>
2022-12-02 10:27:48 -0600

    working on loop exercises

--- js/while.js
+++ js/while.js
@@ -16,8 +16,27 @@ console.log('Running while.js');
 // 16384
 // 32768
 // 65536
+//
+// let numMultiplyBy = 2;
+// let counter = 0;
+// while (counter < 16 ); {
+//     console.log(numMultiplyBy);
+//     numMultiplyBy *= 2;
+//     counter++;
 
+}
+// for (let i = 0, num = 2; i < 16; i++) {
+//     console.log(num);
+//     num *= 2;
+// } the for loop also completes this exercise.
 
+// Shorthand operators
+// num += 2; // num = num +2;
+// num -= 2; // num = num -2;
+// num *=2; // num = num *2;
+// num /= 2; // num = num/2;
+// num++; // num = num +1;
+//num--; // num = num - 1;
 
 // Do-While
 // An ice cream seller can't go home until she sells
@@ -33,13 +52,30 @@ console.log('Running while.js');
 // random numbers for this exercise.
 
 // This is how you get a random number between 50 and 100
-// var allCones = Math.floor(Math.random() * 50) + 50;
+let allCones = Math.floor(Math.random() * 50) + 50;
 // This expression will generate a random number between 1 and 5
-// Math.floor(Math.random() * 5) + 1;
+Math.floor(Math.random() * 5) + 1;
 
 // The output should be similar to the following:
 //
 // 5 cones sold...  // if there are enough cones
 // Cannot sell you 6 cones I only have 3...  // If there are not enough cones
 // Yay! I sold them all! // If there are no more cones
+
+let conesToSell = getRandomNum(50, 100);
+do {
+    let conesToBuy = getRandomNum(1, 5);
+    if (conesToBuy <= conesToSell) {
+        console.log(`${conesToBuy} cones sold...`);
+        conesToSell -= conesToBuy; // conesToSell = conesToSell - conesToBuy;
+
+    } else {
+        console.log(`Cannot sell you ${conesToBuy} cones, I only have ${conesToSell}...`);
+        console.log(`${conesToSell} cones sold...`);
+        conesToSell -= conesToBuy;
+        console.log('Yay! I have sold them all!');
+        break;
+    }
+
+} while (conesToSell > 0);
 
